a30701d9f58f96dc87b9be18b019b05a53200748
Hannah K <Hannahbeitler@yahoo.com>
2026-07-25 15:08:04 -0400

    Updated trivia questions

--- script.js
+++ script.js
@@ -73,9 +73,90 @@ const questions = [
             "High text markup language",
             "Home tool markup language",
              "Hyper text markup language",
-            "Hyper transfer markup language",
+            "Hyper transfer markup language"
 
             ],
             correct: "Hyper text markup language"
-        }
-    ];
+        },
+    {
+        question: "What does CSS stand for?",
+        answers: [
+            "Computer style sheets",
+            "Creative style sheets",
+            "Cascading Style Sheets",
+            "Coding style syntax"
+
+        ],
+        correct: "Cascading Style Sheets"
+    },
+    {
+        question: "Which programming language is used to make web pages interactive?",
+        answers: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "SQL"
+        ],
+        correct: "JavaScript"
+    },
+{
+    question: "Which HTML tag is used to create a hyperlink?",
+    answers: [
+        "link",
+        "url",
+        "a",
+        "href"
+    ],
+    correct: "a"
+},
+{
+    question: "Which Javascript keyword is used to create a variable?",
+    answers: [
+        "Create",
+        "variable",
+        "Define",
+        "let"
+    ],
+    correct: "let"
+},
+{
+    question: "What does IDE stand for?",
+    answers: [
+        "Integrated Development Environment",
+        "Internal Development Environment",
+        "Integrated Design Environment",
+        "Internal Design Environment"  
+    ],
+    correct: "Integrated Development Environment"
+},
+{
+    question: "What does API stand for?",
+    answers: [
+        "Automated programming interface",
+        "Application programming interface",
+        "Application process identifier",
+        "Advanced programming instructions"
+    ],
+    correct: "Application programming interface"
+},
+{
+    question: "What is debugging?",
+    answers: [
+        "Creating a new website",
+        "Designing a user interface",
+        "Finding and fixing errors in code",
+        "Writing new code"
+    ],
+    correct: "Finding and fixing errors in code"
+},
+{
+    question: "Which Javascript funtion displays a popup message?",
+    answers: [
+        "Message",
+        "Alert",
+        "Popup",
+        "Show"
+    ],
+    correct: "Alert"
+},
+];
